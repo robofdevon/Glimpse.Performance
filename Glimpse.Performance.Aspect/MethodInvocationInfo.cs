50693b1a6f770083dc8bb4bb622fb71da1e6accc
Rob Walker <rob@walkernet.org.uk>
2013-07-16 22:43:04 +0100

    Initial commit

---
+++ Glimpse.Performance.Aspect/MethodInvocationInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glimpse.Performance.Aspect
+{
+    public class MethodInvocationInfo
+    {
+        public string MethodName { get; set; }
+        public long InvocationTimeMilliseconds { get; set; }
+    }
+}
