1b718c29650b7d66315b5a8ea599a784fc45e2b3
Rob Walker <rob@walkernet.org.uk>
2013-07-23 14:07:26 +0100

    Style cop changes

---
+++ Glimps.Performance/Aspect/BaseAdvisor.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BaseAdvisor.cs" company="Rob Walker">
+//     Copyright (c) Rob Walker. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Glimpse.Performance.Aspect
+{
+    using PostSharp.Aspects;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    
+    /// <summary>
+    /// Base class for advisors.
+    /// </summary>
+    [Serializable]
+    public abstract class BaseAdvisor : OnMethodBoundaryAspect
+    {
+        /// <summary>
+        /// Gets the Boolean value indicating whether the indicated MethodExecutionArgs relate to a property.
+        /// </summary>
+        /// <param name="args">The MethodExecutionArgs to test.</param>
+        /// <returns>True if the value is a property; otherwise, false.</returns>
+        protected bool IsProperty(MethodExecutionArgs args)
+        {
+            return args.Method.IsSpecialName &&
+                (args.Method.Attributes & MethodAttributes.HideBySig) != 0;
+        }
+    }
+}
--- Glimps.Performance/Aspect/MethodPerformanceAdvisor.cs
+++ Glimps.Performance/Aspect/MethodPerformanceAdvisor.cs
@@ -1,37 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Diagnostics;
-using PostSharp.Aspects;
-using System.Reflection;
-using System.Web;
-using Glimpse.Performance.Interface;
-using Glimpse.Performance.Factory;
-using Glimpse.Performance.Config;
+﻿//-----------------------------------------------------------------------
+// <copyright file="MethodInvocationInfo.cs" company="Rob Walker">
+//     Copyright (c) Rob Walker. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace Glimpse.Performance.Aspect
 {
+    using Glimpse.Performance.Config;
+    using Glimpse.Performance.Factory;
+    using Glimpse.Performance.Interface;
+    using PostSharp.Aspects;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Reflection;
+    using System.Web;
+    using System.Text;
+    
+    /// <summary>
+    /// An advisor which adds performance based method advice to a subject.
+    /// </summary>
     [Serializable]
-    public class MethodPerformanceAdvisor : BasePerformanceAdvisor
+    public class MethodPerformanceAdvisor : BaseAdvisor
     {
+        /// <summary>
+        /// Used to gather timing information.
+        /// </summary>
         static readonly Stopwatch stopwatch = new Stopwatch();
         
-        //cache per request lifetime
+        //Cache variables per request lifetime
+        /// <summary>
+        /// Flags whether the advisor is enabled.
+        /// </summary>
         private bool enabled = false;
+
+        /// <summary>
+        /// Flags whether the enabled flag has been set in this web request.
+        /// </summary>
         private bool enabledSet = false;
+
+        /// <summary>
+        /// Object used for locking, to ensure thread safe access.
+        /// </summary>
         private static object syncRoot = new object();
 
+        /// <summary>
+        /// Initializes static members of the <see cref="MethodPerformanceAdvisor" /> class.
+        /// Starts the stop watch running.
+        /// </summary>
         static MethodPerformanceAdvisor()
         {
+            //enabled? todo
             stopwatch.Start();
         }
 
         /// <summary> 
         /// Method invoked before the execution of the method to which the current 
-        /// aspect is applied. 
+        /// aspect is applied. If enabled = true and the context is not a property,
+        /// store the start time in the args. This instance will be passed onto the OnExit call.
         /// </summary> 
-        /// <param name="args">Unused.</param> 
+        /// <param name="args">The MethodExecutionArgs passed in from the subject method.</param> 
         public override void OnEntry(MethodExecutionArgs args)
         {
             if(! enabledSet)
@@ -46,27 +75,32 @@ namespace Glimpse.Performance.Aspect
                 } 
             }
             
-            //don't log is disabled
-            //don't log properties, this would be too verbose
-            if (enabled && !isProperty(args))
+            //Don't log is disabled
+            //Don't log properties, this would be too verbose
+            if (enabled && !IsProperty(args))
             {
                 args.MethodExecutionTag = stopwatch.ElapsedMilliseconds;
             }
         }
 
         //Cache for request lifetime
+        /// <summary>
+        /// 
+        /// </summary>
         private IStorageProvider storageProvider;
 
         /// <summary> 
         /// Method invoked after the execution of the method to which the current 
-        /// aspect is applied. 
+        /// aspect is applied. If enabled = true and the context is not a property,
+        /// capture the subject method info, including the execution time and store
+        /// the informaton.
         /// </summary> 
-        /// <param name="args">Unused.</param> 
+        /// <param name="args">The MethodExecutionArgs passed in from the subject method.</param> 
         public override void OnExit(MethodExecutionArgs args)
         {
             //Don't log if not in a web request, relying on state
-            //don't log properties, this would be too verbose
-            if (enabled && !isProperty(args))
+            //Don't log properties, this would be too verbose
+            if (enabled && !IsProperty(args))
             {
                 var methodInfo = new MethodInvocationInfo
                 {
